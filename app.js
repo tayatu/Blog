d29989aea825143f41cd9462dbcf4d3c900df277
Sarthak <Sarthak@example.com>
2021-09-04 22:07:27 +0530

    final commit

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/blog"
+    callbackURL: "https://young-gorge-34969.herokuapp.com/auth/google/blog"
   },
   function(accessToken, refreshToken, profile, cb) {
        console.log("hello 2");
